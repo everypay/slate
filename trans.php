360406d55f5ccbff973e5f06c15eda9b4ba03275
Mike Plexousakis <mike.plexousakis@gmail.com>
2015-11-03 19:30:19 +0200

    update trans.php to detect "a href="

--- trans.php
+++ trans.php
@@ -2,8 +2,16 @@
 require_once 'vendor/autoload.php';
 use Behat\Transliterator\Transliterator;
 
-$delimiters = array('h2', 'h1');
-
+$delimiters = array(
+    array('h2', 'id'),
+    array('h1', 'id'),
+    array('a', 'href')
+    );
+//foreach($delimiters as $key){
+//    echo($key[0].' '.$key[1].'="');
+//    echo("\n");
+//}
+//exit;
 //backup original file
 $file = file_get_contents("build/index.html", "r");
 file_put_contents ("build/index0.html", $file);
@@ -14,19 +22,26 @@ if ($handle) {
     while (($line = fgets($handle)) !== false) {
         $found_any_delimiters = 0;
         foreach($delimiters as $key){
-            $test = preg_match('/<'.$key.'/', $line);
+            $test = preg_match('/<'.$key[0].' '.$key[1].'/', $line);
             if($test == 1){
+//echo "found: ".$test.":".$line;
                 $found_any_delimiters = 1;
-                $part = \explode($key.' id="', $line);
+                $part = \explode($key[0].' '.$key[1].'="', $line);
+//print_r($part);
+//echo "count: ".count($part);
                 if(count($part) > 1){
-                    $altered .= $part[0] . $key.' id="';
-                    $subpart = \explode('"', $part[1]);
+                    $altered .= $part[0] . $key[0].' '.$key[1].'="';
+//echo "altered-1: ".$part[0] . $key[0].' '.$key[1].'="';
+                    $subpart = \explode('"', $part[1], 2);
                     if(count($subpart) > 1){
                         $trans = Transliterator::utf8ToAscii($subpart[0]);
                         $altered .= $trans . '"'. $subpart[1];
+//echo "\naltered-2: ".$trans . '"'. $subpart[1];
                     }else{
                         $altered .= $part[1];
+//echo "\naltered-2: ".$part[1];
                     }
+//echo "\n";
                 }else{
                     $altered .= $line;
                 }
